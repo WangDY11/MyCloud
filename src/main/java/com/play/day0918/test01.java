e865fa542c2324b38a004dbc7590256c4ca39c55
wdy <wangdingyu11@163.com>
2022-09-18 20:24:20 +0800

    卷心菜

--- src/main/java/com/play/day0918/test01.java
+++ src/main/java/com/play/day0918/test01.java
@@ -23,15 +23,30 @@ public class test01 {
 
     // 边界条件
     public static int rob(int[] nums) {
+        if (nums.length == 1){
+            return nums[0];
+        }
         int max = 0;
 
+        int dp[][] = new int[nums.length][2];
 
+        // 第一天没偷钱
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
 
-        for (int i = 0; i < nums.length; i++) {
+//        // 第二天没偷钱:昨天偷了钱 和 昨天没偷钱今天也没有偷钱
+//        dp[1][0] = Math.max(dp[0][1], dp[0][0]);
+//        // 第二天偷了钱： 昨天没偷钱+今天偷了钱
+//        dp[1][1] = dp[0][0] + nums[1];
 
+        for (int i = 1; i < nums.length; i++) {
+
+            dp[i][0] = Math.max(dp[i-1][1], dp[i-1][0]);
+
+            dp[i][1] = dp[i-1][0] + nums[i];
+
+            max = Math.max(dp[i][0], dp[i][1]);
         }
-
-
 
         return max;
     }
---
+++ src/main/java/com/play/day0918/test02.java
@@ -0,0 +1,46 @@
+package com.play.day0918;
+
+
+/**
+ * @author wdy
+ * @Description TODO
+ * @createTime 2022年09月18日
+ */
+public class test02 {
+
+    public static void main(String[] args) {
+        int[] n = new int[]{2,1,1,2};
+        System.out.println(rob(n));
+
+    }
+
+
+
+    public static int rob(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1){
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        int a0 = nums[0];
+        int a1 = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            int temp = a1;
+            // 第三天
+            a1 = Math.max(a1, a0 + nums[i]);
+
+            a0 = temp;
+
+        }
+
+        return a1;
+    }
+
+}
