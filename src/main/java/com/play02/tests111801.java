c1d95470717aae91e255f8a768b274d87084ca06
wdy <wdy@e-teng.com.cn>
2022-11-18 17:56:44 +0800

    丑数

---
+++ src/main/java/com/play02/tests111801.java
@@ -0,0 +1,24 @@
+package com.play02;
+
+/**
+ * @author wdy
+ * @Description 丑数
+ * @createTime 2022年11月18日
+ */
+public class tests111801 {
+    // 2 3 5
+    // 1, 2, 3, 4, 5, 6, 8, 9, 10, 12
+    public int nthUglyNumber(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+
+
+        }
+
+        return 0;
+
+
+
+    }
+}
