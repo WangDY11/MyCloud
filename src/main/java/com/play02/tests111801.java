7d5c7d5b3b1baeb7689716cbb227fde59f957463
wdy <wdy@e-teng.com.cn>
2022-11-18 18:05:09 +0800

    丑数

--- src/main/java/com/play02/tests111801.java
+++ src/main/java/com/play02/tests111801.java
@@ -7,18 +7,28 @@ package com.play02;
  */
 public class tests111801 {
     // 2 3 5
-    // 1, 2, 3, 4, 5, 6, 8, 9, 10, 12
+    // 1, 2, 3, 4, 5
+    //
+    // 6  = 2^1*3^1
+    // 8  = 2^3
+    // 10 = 2^1 * 5^1
+    // 12 = 2^2 * 3^1
+    //
     public int nthUglyNumber(int n) {
-
-        for (int i = 1; i <= n; i++) {
+        int dp[] = new int[1691];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 4;
+        dp[5] = 5;
+        if (n <= 5){
+            return dp[n];
+        }
+        for (int i = 6; i <= n; i++) {
 
 
 
         }
-
-        return 0;
-
-
-
+        return dp[n];
     }
 }
