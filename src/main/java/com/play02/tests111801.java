2e1754bf708b0a744ae0adf59d72f1b62efb0606
wdy <wangdingyu11@163.com>
2022-11-20 17:52:55 +0800

    剑指 Offer 49. 丑数

--- src/main/java/com/play02/tests111801.java
+++ src/main/java/com/play02/tests111801.java
@@ -6,28 +6,36 @@ package com.play02;
  * @createTime 2022年11月18日
  */
 public class tests111801 {
+    public static void main(String[] args) {
+        System.out.println(nthUglyNumber(10));
+    }
+
     // 2 3 5
-    // 1, 2, 3, 4, 5
-    //
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
     // 6  = 2^1*3^1
     // 8  = 2^3
+    // 9  = 3^2
     // 10 = 2^1 * 5^1
     // 12 = 2^2 * 3^1
+    // 15 = 3^1 * 5^1
     //
-    public int nthUglyNumber(int n) {
-        int dp[] = new int[1691];
+    public static int nthUglyNumber(int n) {
+        int dp[] = new int[n+1];
         dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        dp[4] = 4;
-        dp[5] = 5;
-        if (n <= 5){
-            return dp[n];
-        }
-        for (int i = 6; i <= n; i++) {
-
-
-
+        int a = 1, b = 1, c = 1;
+        for (int i = 2; i <= n; i++) {
+            int a1 = dp[a] * 2;
+            int b1 = dp[b] * 3;
+            int c1 = dp[c] * 5;
+            int min = Math.min(Math.min(a1, b1), c1);
+            dp[i] = min;
+            if (min == a1) a++;
+            if (min == b1) b++;
+            if (min == c1) c++;
         }
         return dp[n];
     }
