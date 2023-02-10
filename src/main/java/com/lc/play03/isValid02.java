60b4647cf287bf712ed90526dfe2a80ebe056180
wdy <wdy@e-teng.com.cn>
2023-02-10 14:19:26 +0800

    20. 有效的括号

--- src/main/java/com/lc/play03/isValid02.java
+++ src/main/java/com/lc/play03/isValid02.java
@@ -1,14 +1,14 @@
 package com.lc.play03;
 
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  * @author wdy
  * @Description 20. 有效的括号
  * @createTime 2023年02月09日
  */
-public class isValid {
+public class isValid02 {
 
     public static void main(String[] args) {
         System.out.println(isValid("()"));
@@ -25,18 +25,23 @@ public class isValid {
      *  左括号必须以正确的顺序闭合。
      *  每个右括号都有一个对应的相同类型的左括号。
      */
-    //  ascii + stack
+    //  map + stack
     public static boolean isValid(String s) {
         int n = s.length();
         if (n % 2 == 1) {
             return false;
         }
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+
         LinkedList<Character> stack = new LinkedList();
         char[] chars = s.toCharArray();
         for (char c : chars) {
             if (c == '(' || c == '{' || c == '['){
                 stack.push(c);
-            } else if (!stack.isEmpty() && (c - stack.peek() == 1 || c - stack.peek() == 2)){
+            } else if (!stack.isEmpty() && (c == map.get(stack.peek()))){
                 // 防止stack.peek()为空，c - stack.peek()报空指针
                 stack.pop();
             }else {
